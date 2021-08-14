00316a8d2b8b0c68e821cb31a7774c107bb9fdbd
MuhdFazril <67180481+fazril-25Java@users.noreply.github.com>
2021-08-14 12:44:10 +0800

    Add files via upload

---
+++ CPUScheduling.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct Process{
+    int ccode;
+    int duration;
+    int priority;
+    int arrival_time;
+}
+int main(){
+
+    cout<<"hello world"<<endl;
+
+}
